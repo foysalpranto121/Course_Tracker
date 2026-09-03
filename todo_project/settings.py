8d46e531925b075890c678b2bf8c30f583a91753
foysalpranto121 <foysalpranto2002@gmail.com>
2026-09-03 12:41:17 +0600

    requiremnets file create

--- todo_project/settings.py
+++ todo_project/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'todo_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'todo_db',
+        'USER': 'postgres',
+        'PASSWORD': 'pranto121',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
